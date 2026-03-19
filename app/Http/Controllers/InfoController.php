47bb11be6f6d92da16141b42294dfb96df051652
arinazag <arinazagajnova25@gmail.com>
2026-03-19 19:43:44 +0500

    Update InfoController.php

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -1,3 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DTO\ServerInfoDTO;
+use App\DTO\ClientInfoDTO;
+use App\DTO\DatabaseInfoDTO;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class InfoController extends Controller
+{
+    public function serverInfo()
+    {
+        $serverInfo = new ServerInfoDTO(
+            phpVersion: PHP_VERSION,
+            serverSoftware: $_SERVER['SERVER_SOFTWARE'] ?? 'Unknown',
+            phpSapi: php_sapi_name(),
+            maxExecutionTime: (int) ini_get('max_execution_time'),
+            memoryLimit: (int) ini_get('memory_limit')
+        );
+
+        return response()->json($serverInfo);
+    }
+
+    public function clientInfo(Request $request)
+    {
+        $clientInfo = new ClientInfoDTO(
+            ipAddress: $request->ip(),
+            userAgent: $request->userAgent(),
+            requestMethod: $request->method(),
+            requestUri: $request->path()
+        );
+
+        return response()->json($clientInfo);
+    }
+
     public function databaseInfo()
     {
         try {
@@ -5,7 +42,7 @@
             $driver = config("database.connections.$connection.driver");
             $databaseName = config("database.connections.$connection.database");
             
-            // Для MySQL
+
             $versionInfo = DB::select('SELECT VERSION() as version');
             $serverVersion = $versionInfo[0]->version ?? 'Unknown';
             
@@ -23,3 +60,4 @@
 
         return response()->json($databaseInfo);
     }
+}
